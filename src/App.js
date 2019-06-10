652db951dddb574f4b0887da5b8802b495f63610
Makrand S <makrandsanap@gmail.com>
2019-06-10 04:05:29 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> and save to reload (edited  from  C)
+	</p> 
         <a
           className="App-link"
           href="https://reactjs.org"
